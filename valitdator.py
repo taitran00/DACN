5603f6555bd715fd891a881c373ffcaf582f4d5b
taitran00 <95703447+taitran00@users.noreply.github.com>
2021-12-15 15:53:34 +0700

    Add files via upload

---
+++ valitdator.py
@@ -0,0 +1,26 @@
+
+
+# ham cap nhat rule
+from Tools.scripts.generate_opcode_h import header
+
+
+def updateRule (self, idRule, idRuleSnort):
+    from_key = header.signer_public_key
+    # luu lai rule vao validator
+    if idRule == idRuleSnort:
+        idRule +=1
+    else:
+
+
+
+# ham tai rule ve
+def downRule (idRuleSnort, idRule):
+    from_key = header.signer_public_key
+    # luu lai rule vao validator
+    if idRuleSnort == idRule:
+        idRuleSnort += 1
+    else:
+
+
+# ham ghi lai log cua validator
+
